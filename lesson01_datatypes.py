19f761d11188fdc32d87e4066f53a5fc6443bf4e
LeoNHV <villani20298805@student.nhvweb.net>
2025-09-24 16:17:56 +0000

    creates data types

---
+++ lesson01_datatypes.py
@@ -0,0 +1,27 @@
+#This is a comment
+#DATATYPES
+
+
+#1. Strings
+print("hello world")
+print(" ")
+print("hello, \nhow are you \ntoday world?")
+print(Name: \nBilly \nAge: \n14 \nGrade: \t9)
+# INTEGERS
+
+print(67)
+print(-67)
+print(5+5-11)
+#FLOAT
+print(type("To see thismessage, you need to put me in a print statement")
+print(4.0)
+print(2.0+4.5)
+print(2.0+4)
+
+#BOOLEAN
+print(True + True) # 1
+print(False) # 0
+print(1==67)
+print(1!=2)
+
+print("hello" == "Hellooo")

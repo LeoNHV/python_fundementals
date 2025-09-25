a55f1fe2c9b4618a6ca902abf260c9a872db591b
LeoNHV <villani20298805@student.nhvweb.net>
2025-09-25 17:25:50 +0000

    For some challenges

---
+++ lesson02_variables.py
@@ -0,0 +1,48 @@
+#CONCEPTS VARIABLES
+
+taco = "Skibidi"
+
+password = "BlueL0ckW"
+
+
+print("password: ",password)
+
+temperature = 67.82
+print(type(temperature))
+price = 3.99
+
+enabled = False
+is_complete = True
+print(is_complete)
+
+enabled = True 
+print(enabled)
+
+x= 3.14
+y = 7
+print(x + y)
+
+count = 10
+print(count)
+count_down = count - 1
+print(count_down)
+count = count_down
+print(count)
+
+x = "Radia H. Sky Perlman"
+y = 34.53859
+z = "Networking Science Engineer"
+
+count = 10
+count_down = count + 5
+print(count_down)
+x = 4
+y = "hello"
+z = x
+x = y
+y = z
+print(y)
+print(x)
+is_raining = False
+is_raining = True
+print("Is it raining?: ", is_raining)

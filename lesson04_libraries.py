606765c9dfd330e3845a63c54b8d05dd7980243b
LeoNHV <villani20298805@student.nhvweb.net>
2025-09-30 15:53:58 +0000

    math library

---
+++ lesson04_libraries.py
@@ -0,0 +1,14 @@
+# Python libaries are repositories of code.
+
+import math 
+
+print("Square root", math.sqrt(64))
+
+print("Round up ", math.ceil(2.653))
+
+print("Round down ", math.floor(2.9))
+
+print("Power", math.pow (2,3))
+
+print("Pi Value: ", math.pi)
+

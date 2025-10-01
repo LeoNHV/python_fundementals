44769284a255456d6bce661f21edf31678ed04ef
LeoNHV <villani20298805@student.nhvweb.net>
2025-10-01 17:24:21 +0000

    Math Libary2

--- lesson04_libraries.py
+++ lesson04_libraries.py
@@ -12,3 +12,57 @@ print("Power", math.pow (2,3))
 
 print("Pi Value: ", math.pi)
 
+#Random Library
+
+import math
+
+
+seed = 12.4444
+step1 = seed / 6.7
+step2 = step1 - 800
+step3 = step2 + 180843
+result = math.floor(step3)
+limitednumber = step3 - result
+answer = limitednumber
+print(limitednumber)
+
+
+seed = 12.4444
+step1 = seed / 6.7
+step2 = step1 - 800
+step3 = step2 + 180843
+result = math.floor(step3)
+limitednumber = step3 - result
+answer = math.floor(limitednumber * 10)
+
+print(answer)
+
+
+import random
+
+print("Random Integer ", random.randint(1 , 7))
+
+
+
+mylist = ("egg", "cheese", "chicken")
+random.choice("egg", "cheese", "chicken") 
+print(random.choice("egg", "cheese", "chicken"))
+
+print(random.choice(mylist))
+
+
+radius = 7
+
+pi = 3.14
+radius * pi
+
+
+
+dieroll = 12.4444
+step1 = seed / 6.7
+step2 = step1 - 800
+step3 = step2 + 180843
+result = math.floor(step3)
+limitednumber = step3 - result
+answer = math.floor(limitednumber * 10)
+
